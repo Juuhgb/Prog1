f954c511f9fe077f1d558027e4c36fea36bf8e79
Juliana Góes Bello <91296525+Juuhgb@users.noreply.github.com>
2023-08-11 21:50:43 -0300

    Update 6.c

--- LISTA 4/6.c
+++ LISTA 4/6.c
@@ -1,34 +1,33 @@
 #include <stdio.h>
 
 float IMC (float peso, float altura){
-    float imc = peso/(altura * altura);
-    return imc;
+    return peso/(altura * altura);
 }
-const char* tabela (float quilo){
+ void tabela (float quilo){
      if (quilo < 18.5){
-        return "Abaixo do peso";
+        printf("Abaixo do peso");
      }
      if(18.5 <= quilo && quilo <= 24.9){
-        return "com Peso normal";
+        printf("Peso normal");
      }
      if(25 <= quilo && quilo <= 29.9){
-        return "com Sobrepeso";
+        printf("Sobrepeso");
      }
      if(quilo >= 30){
-        return "Obeso";
+        printf("Obeso");
      }
-     return "Valor invalido";
 }
 
-int main(){
-    float imc, peso, altura;
-    const char* tabela1;
+int main(){ 
+    float peso, altura;
+
     printf("Digite seu peso: ");
     scanf("%f", &peso);
     printf("Digite sua altura: ");
     scanf("%f", &altura);
-    imc = IMC(peso, altura);
-    tabela1 = tabela(imc);
 
-    printf("Seu IMC eh de: %.2f. Voce esta %s.", imc, tabela1);
+    printf("Seu IMC eh de: %.2f\n", IMC(peso, altura));
+    tabela(IMC(peso, altura));
+
+  return 0;
 }
